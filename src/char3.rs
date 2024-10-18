d7033545693aef167ea4aaee8a97bdaef184e614
andri <a.fishbyh@e-u.edu.ua>
2024-10-18 13:25:47 +0300

    char3.rs

---
+++ src/char3.rs
@@ -0,0 +1,12 @@
+#[test]
+fn main() {
+    let _f: bool = false;
+
+    let t = false;
+    if !t {
+        println!("Success!");
+        }
+}
+
+//в коді не виконується if !t бо воно в значенні true щоб воно виводило Success потрібно
+//перевести змінну в false
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod tree2;
 mod basic_types;
 mod char1;
 mod char2;
+mod char3;
+mod char4;
 
 fn main() {
     println!("Hello, world!");
