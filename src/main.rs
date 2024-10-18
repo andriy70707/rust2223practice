3c8ed7bc793819e88ce454542153d2135a740e01
andri <a.fishbyh@e-u.edu.ua>
2024-10-18 12:34:32 +0300

    numbers

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
 
 mod variable;
-mod numbers1;
 mod romb;
 mod chapter;
-mod numbers2;
-mod numbers4;
-mod numbers5;
-mod numbers6;
 mod tree;
 mod tree2;
-mod numbers7;
-mod numbers8;
-mod numbers9;
-mod numbers10;
-mod numbers11;
+mod numbers;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/numbers/mod.rs
@@ -0,0 +1,11 @@
+mod numbers1;
+mod numbers2;
+mod numbers3;
+mod numbers4;
+mod numbers5;
+mod numbers6;
+mod numbers7;
+mod numbers8;
+mod numbers9;
+mod numbers10;
+mod numbers11;
