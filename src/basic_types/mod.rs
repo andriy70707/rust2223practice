53c5f5c47e293502b14cd39b4214fad1f38770bf
andri <a.fishbyh@e-u.edu.ua>
2024-10-25 13:09:34 +0300

    expressions.rs

--- src/basic_types/mod.rs
+++ src/basic_types/mod.rs
@@ -1,2 +1,5 @@
 mod numbers;
 mod char_bool_and_unit;
+mod expressions1;
+mod expressions2;
+mod expressions3;
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ mod chapter;
 mod tree;
 mod tree2;
 mod basic_types;
-mod expressions1;
-mod expressions2;
-mod expressions3;
 
 fn main() {
     println!("Hello, world!");
