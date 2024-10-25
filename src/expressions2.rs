13d6c9de778cc123ce247b7f24d1bf5f11b192f7
andri <a.fishbyh@e-u.edu.ua>
2024-10-25 13:07:00 +0300

    expressions2.rs

---
+++ src/expressions2.rs
@@ -0,0 +1,8 @@
+#[test]
+fn main() {
+    let v = 3;
+
+    assert!(v == 3);
+
+    println!("Success!");
+}
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod tree;
 mod tree2;
 mod basic_types;
 mod expressions1;
+mod expressions2;
 
 fn main() {
     println!("Hello, world!");
